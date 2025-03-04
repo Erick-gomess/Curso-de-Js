76acc55867973273ec68caa969f7762d838c63d2
Erick <erickbrilhante6@gmail.com>
2025-03-04 18:54:37 -0300

    a

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 81
+
+console.log(`Sua velocidade foi de ${velocidade}Km/h`)
+if(velocidade > 80){
+    console.log('Você foi multado por excesso de velocidade!')
+}
+
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+
+if(pais == 'Brasil'){
+    console.log('Você é nativo')
+}
+else{
+    console.log('Você é estrangeiro')
+}
