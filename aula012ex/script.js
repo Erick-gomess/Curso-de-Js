8eb4b14dd935c5f318b86ec62d2d295df2706d78
Erick <erickbrilhante6@gmail.com>
2025-03-09 01:19:49 -0300

    atualização do código
    adição da madrugada

--- aula012ex/script.js
+++ aula012ex/script.js
@@ -4,21 +4,35 @@ function carregar(){
     let img = document.querySelector('.foto')
     let hora = new Date()
     hora = hora.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if(hora >= 0 && hora < 12){
+    let minutos = new Date()
+    minutos = minutos.getMinutes()
+
+    if(hora >= 6 && hora < 12){
         //bom dia
         img.src = 'imagens/manha.png'
         fundo.style.backgroundColor = ' #e2cd9f'
-        
+        msg.innerHTML = `Agora são ${hora}:${minutos}<br> Bom dia flor do dia!`
     }
     else if(hora >= 12 && hora <18){
         //boa tarde
         img.src = 'imagens/tarde.png'
         fundo.style.backgroundColor = ' #ffba2f'
+        msg.innerHTML = `Agora são ${hora}:${minutos}<br> Boa tarde!`
     }
-    else{
+    else if(hora >= 18 && hora <= 23 ){
         //boa noite
         img.src = 'imagens/noite.png'
         fundo.style.backgroundColor = ' #141414'
+        msg.innerHTML = `Agora são ${hora}:${minutos}<br> Boa noite!`
+    }
+    else{
+        msg.innerHTML = `Agora são ${hora}:${minutos}<br> Madrugando rapaz?`
+        img.src = 'imagens/noite.png'
+        fundo.style.backgroundColor = 'rgb(0, 0, 0)'
+    }
+
+    if(hora == 0){
+        msg.innerHTML = `Agora são ${hora}${hora}:${minutos}<br> Madrugando rapaz?`
+        //pra ficar igual ao relógio, que meia noite marca '00:00'
     }
 }
