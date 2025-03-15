da8849ba67218d916bebec92fc39e54b87b4110a
Erick <erickbrilhante6@gmail.com>
2025-03-15 17:02:17 -0300

    .

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+/*estrutura de repetição com teste lógico no início
+let i = 0
+while(i <= 10){
+    console.log(`número ${i}`)
+    i++
+}
+*/
+
+
+//estrutura de repetição com teste lógico no fim
+let i = 0
+do{
+    console.log(`número ${i}.`)
+    i++
+}while(i <= 10)
